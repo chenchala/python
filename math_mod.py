bb3aa0122828fb608053325cf2a562fdbed3c30f
chenchala <66762422+chenchala@users.noreply.github.com>
2025-07-17 17:54:01 +0530

    Create math_mod.py

---
+++ math_mod.py
@@ -0,0 +1,17 @@
+import math
+from math import sin, pi
+from math import sqrt
+
+a = int(input("Enter a number: "))
+
+#square root
+sqrt = sqrt(a)
+print("Square root: ", sqrt)
+
+#Logarithm
+log = math.log(a)
+print("Logarithm: ", log)
+
+#Sine
+sine_value = math.sin(a)
+print("Sine :", sine_value)
